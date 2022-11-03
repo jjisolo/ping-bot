cd27d687c52cd9559873bf9d44f892fb4041a77a
jjsolo <jjsolo24@gmail.com>
2022-11-03 20:19:56 +0300

    add warnings, fix identation

--- telegram/ping_bot_base.py
+++ telegram/ping_bot_base.py
@@ -1,9 +1,11 @@
-from aiogram                                                                    import Bot, Dispatcher
+from aiogram                            import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher                                           import FSMContext
-from aiogram.dispatcher.filters.state                import State, StatesGroup
+from aiogram.dispatcher                 import FSMContext
+from aiogram.dispatcher.filters.state   import State, StatesGroup
 
-import os, aiogram, logging, json
+import os, aiogram, logging, json, warnings
+
+WARNING_CRITICAL_HIT = "The program experienced critical error. Consider seeing log/dev_log.log to identify the error."
 
 logging.basicConfig(
         format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
@@ -33,3 +35,12 @@ with open("resource/message_templates.json") as json_file:
 
 with open("resource/message_log_meta.json") as json_file:
     log_meta          = json.load(json_file)
+
+def commit_critical_error(log_message: str) -> None:
+    dev_logger.critical(log_message)
+    warnings.warn(WARNING_CRITICAL_HIT, RuntimeWarning)
+    raise SystemExit # TODO register exit call in exit-chain?            
+
+
+   
+
