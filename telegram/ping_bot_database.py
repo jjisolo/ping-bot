c792490f035d9df008393d3d87ce68f39031e62f
jjsolo <jjsolo24@gmail.com>
2022-11-03 20:20:42 +0300

    write tests for database error-cheking

--- telegram/ping_bot_database.py
+++ telegram/ping_bot_database.py
@@ -1,4 +1,6 @@
-from ping_bot_base.py import dev_logger, usr_logger
+from telegram.ping_bot_base import dev_logger, usr_logger, commit_critical_error, WARNING_CRITICAL_HIT
+
+import sqlite3, psutil, typing, os, warnings
 
 class ping_bot_database_manager(object):
     """
@@ -12,6 +14,42 @@ class ping_bot_database_manager(object):
 
         :param databse_filename(str): filename of the database.
         """
-
+        dev_logger.debug(("Initializing sqlite3 database"))
         
+        self.__assert_file_exists(database_filename)
+        self.connection = sqlite3.connect(database_filename)
+        self.__assert_connection__(database_filename)
+        
+        self.cursor = self.connection.cursor()
+    
+    @classmethod # For Unit-Testing.
+    def __assert_file_exists__(cls, database_filename) -> None:
+        """
+        Check if the requested file exists, if not report an error
+        to the dev logging pipe.
+        """
+        result = os.path.exists(database_filename)
 
+        if not result:
+            commit_critical_error("Requested database file does not exist.")
+        else:
+            dev_logger.info("Performed database file existing check.")
+    
+    @classmethod # For Unit-Testing.
+    def __assert_connection_established__(cls, database_filename: str) -> None:
+        """
+        Check if *something* is connected to the database, if not
+        report an error to the dev logging pipe.
+        """
+        for procedure in psutil.process_iter():
+            try:
+                files = procedure.open_files()
+                if files:
+                    for file in files:
+                        if file.path == database_filename:
+                            return
+            except psutil.NoSuchProcess as error:
+                commit_critical_error("Expirienced psutil error:", error)
+
+        commit_critical_error("No connection to the database has been established.")
+ 
